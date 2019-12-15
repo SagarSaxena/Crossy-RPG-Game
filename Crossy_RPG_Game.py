83ad5ed9616dcaa7125bd46bf4f18ffd739a55d2
SagarSaxena <sagar.saxena@alumni.stanford.edu>
2019-12-14 23:04:47 -0500

    Started making code object oriented. Created game class

--- Crossy_RPG_Game.py
+++ Crossy_RPG_Game.py
@@ -1,54 +1,66 @@
-# Pygame development 3
-# Draw 
-#Set up the display
-
 import pygame
 
-pygame.init()
-
+SCREEN_TITLE = "Crossy RPG"
 #size of the screen
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-SCREEN_TITLE = "Crossy RPG"
 #Colors according to RGB codes (tuples)
 WHITE_COLOR = (255, 255, 255) 
 BLACK_COLOR = (0, 0, 0)
 clock = pygame.time.Clock()
-TICK_RATE = 60
-is_game_over = False
 
-# create window of specified size
-game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-# set background of window to white
-game_screen.fill(WHITE_COLOR)
-pygame.display.set_caption(SCREEN_TITLE)
+class Game:
+    
+    TICK_RATE = 60
+    is_game_over = False
 
-player_image = pygame.image.load('player.png')
-player_image = pygame.transform.scale(player_image, (50, 50))
+    # initializer for game class to set up width, height and title
+    def __init__(self, title, width, height):
+        self.title = title
+        self.width = width
+        self.height = height
 
-# Main game loop, used to update all gameplay such as movement, checks, and graphics
-# Runs until is_game_over = True
-while not is_game_over:
+        # create window of specified size
+        self.game_screen = pygame.display.set_mode((width, height))
+        # set background of window to white
+        self.game_screen.fill(WHITE_COLOR)
+        pygame.display.set_caption(title)
 
-    # A loop to get all of the events occuring at any given time
-    # Events are most often mouse movement, mouse and button clicks, or exit events
-    for event in pygame.event.get():
-        #If we have a quit type evene,t then exit out of the game loop
-        if event.type == pygame.QUIT:
-            is_game_over = True
-        print(event)
+    def run_game_loop(self):
+        is_game_over = False
 
-    #pygame.draw.rect(game_screen,BLACK_COLOR, [350, 350, 100, 100])
-    #pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
+        # Main game loop, used to update all gameplay such as movement, checks, and graphics
+        # Runs until is_game_over = True
+        while not is_game_over:
+
+            # A loop to get all of the events occuring at any given time
+            # Events are most often mouse movement, mouse and button clicks, or exit events
+            for event in pygame.event.get():
+                #If we have a quit type evene,t then exit out of the game loop
+                if event.type == pygame.QUIT:
+                    is_game_over = True
+                print(event)
+                
+            # Update all game graphics        
+            pygame.display.update()
+            # Tick the clock to update eveything within the game
+            clock.tick(self.TICK_RATE)
 
 
-    game_screen.blit(player_image, (375,375))
-        
-    # Update all game graphics        
-    pygame.display.update()
-    # Tick the clock to update eveything within the game
-    clock.tick(TICK_RATE)
+pygame.init()
 
-#Quite pygame and the program
+new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game.run_game_loop()
+
+#Quit pygame and the program
 pygame.quit()
 quit()
+
+
+#player_image = pygame.image.load('player.png')
+#player_image = pygame.transform.scale(player_image, (50, 50))
+
+#pygame.draw.rect(game_screen,BLACK_COLOR, [350, 350, 100, 100])
+#pygame.draw.circle(game_screen, BLACK_COLOR, (400, 300), 50)
+
+#game_screen.blit(player_image, (375,375))
