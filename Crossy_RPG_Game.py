b4549596263c645cbc1458d8ed9e99a940d04e02
SagarSaxena <sagar.saxena@alumni.stanford.edu>
2019-12-16 23:22:03 -0500

    Implemented Player class and basic movement

--- Crossy_RPG_Game.py
+++ Crossy_RPG_Game.py
@@ -2,8 +2,8 @@ import pygame
 
 SCREEN_TITLE = "Crossy RPG"
 #size of the screen
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 #Colors according to RGB codes (tuples)
 WHITE_COLOR = (255, 255, 255) 
 BLACK_COLOR = (0, 0, 0)
@@ -28,6 +28,9 @@ class Game:
 
     def run_game_loop(self):
         is_game_over = False
+        direction = 0
+
+        player_character = PlayerCharacter('ironman.png', 275, 550, 150, 150)
 
         # Main game loop, used to update all gameplay such as movement, checks, and graphics
         # Runs until is_game_over = True
@@ -36,10 +39,31 @@ class Game:
             # A loop to get all of the events occuring at any given time
             # Events are most often mouse movement, mouse and button clicks, or exit events
             for event in pygame.event.get():
+
                 #If we have a quit type evene,t then exit out of the game loop
                 if event.type == pygame.QUIT:
                     is_game_over = True
+
+                # Detect when key is pressed down
+                elif event.type == pygame.KEYDOWN:
+                    # Move up if up key pressed
+                    if event.key == pygame.K_UP:
+                        direction = 1
+                    # Move down if down key pressed
+                    elif event.key == pygame.K_DOWN:
+                        direction = -1
+                        
+                # Detect when key is released
+                elif event.type == pygame.KEYUP:
+                    # Stop movement when key no longer pressed
+                    if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        direction = 0
+                    
                 print(event)
+
+            self.game_screen.fill(WHITE_COLOR)
+            player_character.move(direction)
+            player_character.draw(self.game_screen)
                 
             # Update all game graphics        
             pygame.display.update()
@@ -48,7 +72,7 @@ class Game:
 
 class GameObject:
     
-    def __init__(self, x, y, image_path, width, height):
+    def __init__(self, image_path, x, y, width, height):
 
         object_image = pygame.image.load(image_path)
         self.image = pygame.transform.scale(object_image, (width, height))
@@ -56,9 +80,28 @@ class GameObject:
         self.x_pos = x
         self.y_pos = y
 
+        self.width = width
+        self.height = height
+
+    #Draw the object by "blitting" it onto the background
     def draw(self, background):
         background.blit(self.image, (self.x_pos, self.y_pos))
 
+# Class to represent the character controlled by the player
+class PlayerCharacter(GameObject):
+
+    # How many tiles the character moves per second
+    SPEED = 10
+    
+    def __init__(self, image_path, x, y, width, height):
+        super().__init__(image_path, x, y, width, height)
+
+    # Move function move character up if direction > 0 and down if < 0
+    def move(self, direction):
+        if direction > 0:
+            self.y_pos -= self.SPEED
+        elif direction < 0:
+            self.y_pos += self.SPEED
 
 pygame.init()
 
